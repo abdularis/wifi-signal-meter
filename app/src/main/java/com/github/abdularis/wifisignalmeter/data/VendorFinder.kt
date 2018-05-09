8930502ab28bfd9302caaed394081cd77a532c97
Abdul Aris <abdularisrahmanudin10@gmail.com>
2018-05-09 18:27:42 +0700

    Add lru cache into vendor finder module

--- app/src/main/java/com/github/abdularis/wifisignalmeter/data/VendorFinder.kt
+++ app/src/main/java/com/github/abdularis/wifisignalmeter/data/VendorFinder.kt
@@ -1,6 +1,7 @@
 package com.github.abdularis.wifisignalmeter.data
 
 import android.content.Context
+import android.support.v4.util.LruCache
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper
 
 const val DB_NAME = "standard_ieee_oui.db"
@@ -13,16 +14,24 @@ class DbHelper(ctx: Context): SQLiteAssetHelper(ctx, DB_NAME, null, DB_VERSION)
 
 class VendorFinder(private val dbHelper: DbHelper) {
 
+    private val cache: LruCache<String, String> = LruCache(32)
+
     fun findVendor(mac: String): String? {
+        var vendor: String? = cache.get(mac)
+
+        if (vendor != null) {
+            return vendor
+        }
+
         val vendorId = mac.substring(0, 8).toUpperCase()
         val db = dbHelper.readableDatabase
         val cursor = db.query(TABLE_NAME, arrayOf(COL_VENDOR), "$COL_MAC=?", arrayOf(vendorId), null, null, null)
-        var vendor: String? = null
         if (cursor.moveToFirst()) {
             vendor = cursor.getString(0)
         }
         cursor.close()
 
+        cache.put(mac, vendor)
         return vendor
     }
 
